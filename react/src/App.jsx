cf49ae3a70048aba5576deb95bc393ea04b39c50
Nassanalytics <nassim.naoui@gmail.com>
2025-03-21 00:10:40 +0100

    Ajout feature display education CV model

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -11,6 +11,23 @@ export default function App() {
   const [phoneNumber, setPhoneNumber] = useState("+44 3245 5521 5521");
   const [address, setAddress] = useState("New York, USA");
 
+  const [education, setEducation] = useState([
+    {
+      name: "IFAG",
+      degree: "Master",
+      startDate: "09/2016",
+      endDate: "09/2018",
+      location: "Boulogne",
+    },
+    {
+      name: "CNAM",
+      degree: "Bachelor",
+      startDate: "09/2013",
+      endDate: "09/2016",
+      location: "Paris 3",
+    },
+  ]);
+
   const [schoolName, setSchoolName] = useState(["Ifag", "CNAM", "Evariste"]);
   const [degree, setDegree] = useState(["Master", "Bachelor", "Baccalaureate"]);
 
@@ -55,11 +72,19 @@ export default function App() {
           email={email}
           phoneNumber={phoneNumber}
           address={address}
-          schoolName={schoolName.map((schoolName) => (
-            <li key={schoolName}>{schoolName}</li>
-          ))}
-          degree={degree.map((degree) => (
-            <li key={degree}>{degree}</li>
+          schoolName={education.map((school) => (
+            <div className="education-field">
+              <div className="date-location">
+                <div id="date">
+                  {school.startDate} {school.endDate}
+                </div>
+                <div id="location">{school.location}</div>
+              </div>
+              <div className="school-degree">
+                <div id="school">{school.name}</div>
+                <div id="degree">{school.degree}</div>
+              </div>
+            </div>
           ))}
         />
       </div>
